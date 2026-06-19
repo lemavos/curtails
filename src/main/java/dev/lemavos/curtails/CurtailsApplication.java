45081339f8551c67c41de8d403d885c606e9d6d5
lemavos <lemxvos.dev@proton.me>
2026-06-19 20:20:46 -0300

    first commit

---
+++ src/main/java/dev/lemavos/curtails/CurtailsApplication.java
@@ -0,0 +1,13 @@
+package dev.lemavos.curtails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurtailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lemavos/curtails/CurtailsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lemavos.curtails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurtailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
